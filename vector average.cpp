519a228d91a4eba8c159b5d4572d8cb06ccad544
haohua <haohuafeng08@gmail.com>
2021-10-13 19:55:17 -0400

    Initial commit

---
+++ vector average.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    vector<int> vector;
+    int user_input;
+    while(true){
+        cout<<"Enter the your number: ";
+        cin>>user_input;
+        if(user_input>=0){
+            break;
+        }
+        vector.push_back(user_input);
+    }
+    if(vector.empty()) {
+        cout << "Vector is empty, abort!";
+        return 0;
+    }
+    float sum = 0;
+    for (int i: vector) {
+        sum += i;
+    }
+    cout << "\nThe average of all number you entered in the vector is " << sum / int(vector.size());
+    return 0;
+};
