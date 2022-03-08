16b9901358d5487542c20530b7b98ac4471f73df
Haohua Feng <haohuafeng08@gmail.com>
2022-03-07 19:29:33 -0500

    add assignments

--- read file/cmake-build-debug/CMakeFiles/c___review.dir/DependInfo.cmake
+++ read file/cmake-build-debug/CMakeFiles/c___review.dir/DependInfo.cmake
@@ -4,20 +4,14 @@ set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
 
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
-  "CXX"
-  )
-# The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
-  "/cygdrive/e/c++ review/read file/text_file_reading.cpp" "/cygdrive/e/c++ review/read file/cmake-build-debug/CMakeFiles/c___review.dir/text_file_reading.cpp.o"
-  )
-set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
   )
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/c++ review/circlar sorted linked list/circular_sorted_linked_list.cpp" "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/circular_sorted_linked_list.cpp.obj" "gcc" "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/circular_sorted_linked_list.cpp.obj.d"
+  "E:/c++ review/circlar sorted linked list/main.cpp" "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/main.cpp.obj" "gcc" "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/main.cpp.obj.d"
+  "E:/c++ review/date class/driver.cpp" "CMakeFiles/c___review.dir/E_/c++_review/date_class/driver.cpp.obj" "gcc" "CMakeFiles/c___review.dir/E_/c++_review/date_class/driver.cpp.obj.d"
+  "E:/c++ review/read file/text_file_reading.cpp" "CMakeFiles/c___review.dir/text_file_reading.cpp.obj" "gcc" "CMakeFiles/c___review.dir/text_file_reading.cpp.obj.d"
   )
 
 # Targets to which this target links.
--- read file/cmake-build-debug/CMakeFiles/c___review.dir/cmake_clean.cmake
+++ read file/cmake-build-debug/CMakeFiles/c___review.dir/cmake_clean.cmake
@@ -1,6 +1,14 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/c___review.dir/text_file_reading.cpp.o"
+  "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/circular_sorted_linked_list.cpp.obj"
+  "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/circular_sorted_linked_list.cpp.obj.d"
+  "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/main.cpp.obj"
+  "CMakeFiles/c___review.dir/E_/c++_review/circlar_sorted_linked_list/main.cpp.obj.d"
+  "CMakeFiles/c___review.dir/E_/c++_review/date_class/driver.cpp.obj"
+  "CMakeFiles/c___review.dir/E_/c++_review/date_class/driver.cpp.obj.d"
+  "CMakeFiles/c___review.dir/text_file_reading.cpp.obj"
+  "CMakeFiles/c___review.dir/text_file_reading.cpp.obj.d"
   "c___review.exe"
+  "c___review.exe.manifest"
   "c___review.pdb"
   "libc___review.dll.a"
 )
